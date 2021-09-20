0604c9c8da4d42a25a6985e991fbe361220e1d5c
shizhou <shiz@youxin.cloud>
2021-09-20 13:45:43 +0800

    format错误修改

--- getYoutube.py
+++ getYoutube.py
@@ -286,7 +286,7 @@ def get_QR_doe():
     except Exception:
         print("没有元素")
     png = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
-    driver.get_screenshot_as_file(r"$s/png/%s.png" % (youtubeDir,png))
+    driver.get_screenshot_as_file("%s/png/%s.png" % (youtubeDir, png))
 
     get_ewm(youtubeDir + "/png/" + png + ".png")
     t = threading.Timer(30, get_QR_doe)
